c6924e9c38a7678892121729208a09bfd2291212
devajsofficial <93475779+devajsofficial@users.noreply.github.com>
2024-04-20 21:05:30 +0200

    Update cfg.lua

--- cfg.lua
+++ cfg.lua
@@ -1,13 +1,13 @@
 DrawDistance = 30
 
-Zakljucano = false
+Zakljucano = true --locked
 
 Auta = {
     {
-        model = 'sultan',
-        Lokacija = vec(214.3503, -799.673, 30.837),
+        model = 'sultan', --car mod
+        Lokacija = vec(214.3503, -799.673, 30.837), --loc
         heading = 215.40,
-        okrecese = false,
-        tablice = "TEST"
+        okrecese = true, --rotating
+        tablice = "DEVAJS" --plates
 }
 }
