e2736f184b327cbd8408340b4ece15564e9dc572
devayee1 <93475779+devajsofficial@users.noreply.github.com>
2025-02-22 22:13:51 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,6 +1,11 @@
 fx_version 'cerulean'
 game 'gta5'
 
+author 'devayee'
+rep 'https://github.com/devajsofficial/vehiclespawner/tree/main'
+version '1.0.0'
+
+
 client_scripts {
 'cfg.lua',
 'test.lua'
